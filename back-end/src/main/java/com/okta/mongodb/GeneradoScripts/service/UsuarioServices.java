65262d6166311925c0a9cdc9d252a8c57afa721a
Luis Lara <xanderls25@icloud.com>
2025-04-18 13:43:02 -0500

    Update the UsuarioServices.java file in the back-end

--- back-end/src/main/java/com/okta/mongodb/GeneradoScripts/service/UsuarioServices.java
+++ back-end/src/main/java/com/okta/mongodb/GeneradoScripts/service/UsuarioServices.java
@@ -3,9 +3,7 @@ package com.okta.mongodb.GeneradoScripts.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Optional;
 
